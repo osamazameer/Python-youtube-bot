7c539774358d3c01cf437c3473e0d4fabf98ee93
osamazamir <ocamahere@gmail.com>
2021-01-12 21:14:17 +0500

    Test Youtube bot

---
+++ main.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+
+
+driver = webdriver.Chrome()
+driver.get('https://www.youtube.com')      
+
+driver.implicitly_wait(10)
+
+#Adds texts to Search Field
+searchbox = driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div/div[1]/input')
+searchbox.send_keys('Ocamas Effects')
+
+#Clicks Search Button
+searchbutton = driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/button')
+searchbutton.click()
+
+#Clicks Channel
+driver.implicitly_wait(10)
+driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-channel-renderer[1]/div/div[1]/a/div/yt-img-shadow/img').click()
+
+#Clicks Videos
+driver.implicitly_wait(10)
+driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-browse/div[3]/ytd-c4-tabbed-header-renderer/app-header-layout/div/app-header/div[2]/app-toolbar/div/div/paper-tabs/div/div/paper-tab[2]').click()
+
+#Clicks Specific Video
+driver.implicitly_wait(5)
+driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-renderer/div[1]/ytd-grid-video-renderer[2]/div[1]/ytd-thumbnail').click()
